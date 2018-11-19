331c4f95698c148dae26f6ed5bbc01fc90d68254
chipdelmal <chipdelmal@gmail.com>
2018-11-19 10:16:22 -0800

    Routines to match filenames are done, just need to do the replaces/merges in the files

--- functions.py
+++ functions.py
@@ -16,25 +16,36 @@ import glob
 
 
 def readFilepaths(experimentPath, extension=".fa"):
-    """
-    Description:
-    In:
-    Out:
-    """
-    filepaths = sorted(
-        glob.glob(
-            experimentPath + "/" + "*" + extension
-        )
-    )
-    return filepaths
+    filepaths = glob.glob(experimentPath + "/" + "*" + extension)
+    return sorted(filepaths)
+
 
 def getFilenamesFromPaths(filepaths):
-    """
-    Description:
-    In:
-    Out:
-    """
-    filenames=[None]*len(filepaths)
+    filenames = [None]*len(filepaths)
     for i, path in enumerate(filepaths):
-        filenames[i]=path.split("/")[-1]
+        filenames[i] = path.split("/")[-1]
     return filenames
+
+
+def getUniqueIdentifiers(filenames, separator="_"):
+    nonUniqueID = [None]*len(filenames)
+    for i, name in enumerate(filenames):
+        nonUniqueID[i] = name.split("_")[0]
+    uniqueID = set(nonUniqueID)
+    return sorted(list(uniqueID))
+
+
+def checkIdentifierForMatches(filenames, identifier):
+    matchingIx = []
+    for i, name in enumerate(filenames):
+        if identifier in filenames[i]:
+            matchingIx.append(i)
+    return matchingIx
+
+
+def getIdentifierToFilenamesMatches(filenames, identifiers):
+    matchedIndices = []
+    for id in identifiers:
+        matchingIndices = checkIdentifierForMatches(filenames, id)
+        matchedIndices.append(matchingIndices)
+    return matchedIndices
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-#
-#
+#############################################################################
+# Main routine for the ad hoc fasta files parser
+# Description:
+#############################################################################
 
 import functions as parser
 
 PATH = "/Users/sanchez.hmsc/Documents/GitHub/fastaSpacerHeaderParser/Data"
-filepaths = parser.readFilepaths(PATH,extension=".fa")
-parser.getFilenamesFromPaths(filepaths)
+
+filepaths = parser.readFilepaths(PATH, extension=".fa")
+filenames = parser.getFilenamesFromPaths(filepaths)
+identifiers = parser.getUniqueIdentifiers(filenames, separator="_")
+matchedIndices = parser.getIdentifierToFilenamesMatches(filenames, identifiers)
+
+
+file = open(filepaths[0], "r")
+read=file.read()
+read.replace(">spacer","")
